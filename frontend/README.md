9b1d4451b353e4d50449ed54c24ae5ec7a3a207b
TylerSelden <tyler.selden@salesianum.org>
2023-06-05 17:41:18 -0400

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,16 @@
+# Stock Market Game 3
+The third edition of Stock Market Game, with new features and an updated UI.
+
+## New Features
+- Slick new UI that  makes playing much more fun and simple
+- Graphs that help players know when it's a good time to buy or sell
+- Fees when you buy shares
+- A **Master Graph** that allows players to see overall trends of companies
+- Transactions, which allow players to make deals with each other
+- A simple leaderboard including players' net worth
+- **Staking**, which allows players to freeze a certain amount of money for 24 hours, and get it back plus interest
+
+## A special thanks to:
+- Nessie
+- Sean
+- (Salty) Saulita
---
+++ frontend/script.js
@@ -0,0 +1,57 @@
+var Screens = {
+  market: {
+    id: "market-screen",
+    header: "The Market",
+    element: null
+  },
+  graph: {
+    id: "graph-screen",
+    header: "Master Graph",
+    element: null
+  },
+  transactions: {
+    id: "transactions-screen",
+    header: "Transactions",
+    element: null
+  },
+  leaderboard: {
+    id: "leaderboard-screen",
+    header: "Leaderboard",
+    element: null
+  }
+}
+
+var Buttons = {
+  notifications: {
+    show: function() {
+      document.getElementById("notifications").style.display = "block";
+      document.getElementById("notifications-outclick").style = "width: 100vw; height: 100vh;";
+    },
+    hide: function() {
+      document.getElementById("notifications").style.display = "none";
+      document.getElementById("notifications-outclick").style = "width: 0; height: 0;"
+      document.getElementById("messages-icon").src = "images/messages.png";
+    }
+  }
+}
+
+
+window.onload = function() {
+  for (var i in Screens) {
+    Screens[i].element = document.getElementById(Screens[i].id);
+  }
+}
+
+
+function showScreen(name) {
+  document.getElementById("header").innerText = Screens[name].header;
+  for (var i in Screens) {
+    Screens[i].element.style = "display: none;";
+  }
+  Screens[name].element.style = "";
+}
+
+
+// if screen is < 10x8, css will fail
+
+//don't allow decimals in inputs
